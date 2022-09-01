563329a6cac49bdee01f0c7c0c5b32a938ab8911
Ruffles182 <l.salvador.arreola@gmail.com>
2022-09-01 14:44:41 -0500

    se agrego comando del sistema

--- checkIFExist.py
+++ checkIFExist.py
@@ -1,7 +1,13 @@
+import os
 
-nombreArchivo = "test.txt"
-cuenta = 1
-with open(nombreArchivo, 'r') as f:
-  for line in f:
-    print(line + " -- " + str(cuenta))
-    cuenta += 1
+if os.name == 'nt':
+  
+  nombreArchivo = "test.txt"
+  cuenta = 1
+  with open(nombreArchivo, 'r') as f:
+    for line in f:
+      cadena = 'wget -O/dev/null -q ' + line + ' && echo""  || echo ' + str(cuenta)
+      # print(line + " -- " + str(cuenta))
+      cuenta += 1
+  else:
+    print("Upsss, este script está diseñado para linux... :v")
