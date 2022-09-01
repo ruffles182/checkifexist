fbafddd27d7dd2649fad5d68108bf458cb8b7b40
Ruffles182 <l.salvador.arreola@gmail.com>
2022-09-01 15:04:16 -0500

    se agregó la cadena de sistema

--- checkIFExist.py
+++ checkIFExist.py
@@ -6,9 +6,10 @@ if os.name != 'nt':
   cuenta = 1
   with open(nombreArchivo, 'r') as f:
     for line in f:
-      cadena = 'wget -O/dev/null -q ' + line + ' && echo""  || echo ' + str(cuenta)
-      os.system(cadena)
-      # print(line + " -- " + str(cuenta))
+      if line != "":
+        cadena = 'wget -O/dev/null -q ' + line + ' && echo""  || echo ' + str(cuenta)
+        os.system(cadena)
+        # print(line + " -- " + str(cuenta))
       cuenta += 1
 else:
   print("Upsss, este script está diseñado para linux... :v")
