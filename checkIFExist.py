1ecf33ee6bf8b6b54768383ea8df3324bb1c4ec0
Ruffles182 <l.salvador.arreola@gmail.com>
2022-09-01 15:40:47 -0500

    se agregó la cadena de sistema

--- checkIFExist.py
+++ checkIFExist.py
@@ -1,16 +1,42 @@
 import os
+import urllib2
 
-if os.name != 'nt':
+# if os.name != 'nt':
   
-  nombreArchivo = "test.txt"
-  cuenta = 1
-  with open(nombreArchivo, 'r') as f:
-    for line in f:
-      if line != "":
-        cadena = 'wget -O/dev/null -q ' + line
-        if not os.system(cadena):
-          print(str(cuenta))
-        # print(line + " -- " + str(cuenta))
-      cuenta += 1
-else:
-  print("Upsss, este script está diseñado para linux... :v")
+nombreArchivo = "test.txt"
+cuenta = 1
+with open(nombreArchivo, 'r') as f:
+  for line in f:
+    
+    
+    if check(line):
+      print(str(cuenta))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # if line != "":
+
+
+
+
+      # cadena = 'wget -O/dev/null -q ' + line
+      # if not os.system(cadena):
+      #   print(str(cuenta))
+      # print(line + " -- " + str(cuenta))
+    cuenta += 1
+# else:
+#   print("Upsss, este script está diseñado para linux... :v")
+
+def check(url):
+  try:
+    urllib2.urlopen(url)
+    return True
+  except urllib2.HTTPError:
+    return False
